dd6e9a45ec8b5c7604730eafb55c9389a9803137
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2020-02-28 10:59:40 +0100

    get completed for ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,22 +1,42 @@
 from doubly_linked_list import DoublyLinkedList
+'''
+A ring buffer is a non-growable buffer with a fixed size. When the ring buffer is full and a new element is inserted, the oldest element in the ring buffer is overwritten with the newest element. This kind of data structure is very useful for use cases such as storing logs and history information, where you typically want to store information up until it reaches a certain age, after which you don't care about it anymore and don't mind seeing it overwritten by newer data.
 
+Implement this behavior in the RingBuffer class. RingBuffer has two methods, `append` and `get`. The `append` method adds elements to the buffer. The `get` method, which is provided, returns all of the elements in the buffer in a list in their given order. It should not return any `None` values in the list even if they are present in the ring buffer.
+'''
 
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.current = None
         self.storage = DoublyLinkedList()
+        
 
     def append(self, item):
-        pass
+        if self.storage.length < self.capacity:
+            node = self.current
+            self.storage.move_to_end(node)
+            return
+        if self.storage.length == self.capacity:
+            del self.storage.head.value[0]
+            self.storage.remove_from_head()
+        
+        self.storage.add_to_tail(self.current)
+        self.current = self.storage.tail
+      
+    
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
-
+        node = self.storage.head
+        while node != None:
+            list_buffer_contents.append(node.value)
+            node = node.next
         return list_buffer_contents
+
 
 # ----------------Stretch Goal-------------------
 
